4390ac371390a550e1c7831bcb077c7e84d4ea8e
probablysleeping <harker.phillip@gmail.com>
2025-05-08 16:10:18 +0100

    trying to get uvs lol

--- scenes/HexScene/hexagon.gd
+++ scenes/HexScene/hexagon.gd
@@ -4,17 +4,21 @@ extends Node3D
 
 var hex_position : HexHelper.HexCoordinate
 
-
+var HexRenderer = preload("res://scenes/HexScene/hexagon_renderer.gd")
 
 func _ready():
 	var reconstructed = HexHelper.HexCoordinate.from_carthesian(hex_position.to_carthesian())
 	#reconstructed.round_coord()
 	#print(hex_position.to_string(), reconstructed.to_string())
 	
+	
+	var mesh_instance = MeshInstance3D.new()
+	mesh_instance.mesh = ArrayMesh.new()  # force a new mesh
+	mesh_instance.set_script(HexRenderer)  # attach your renderer script
+	add_child(mesh_instance)
+	
 	#$CanvasLayer/Label.text = hex_position.to_string()
-	var uv =  $MeshInstance3D.uv_offset
-	print(uv)
-	$CanvasLayer/Label.text = str(uv)
+	$CanvasLayer/Label.text = str(mesh_instance.uv_offset)
 	
 	#pos update
 	global_position = hex_position.to_carthesian() + Vector3.UP * height
--- scenes/HexScene/hexagon_renderer.gd
+++ scenes/HexScene/hexagon_renderer.gd
@@ -2,43 +2,60 @@ extends MeshInstance3D
 
 var normalizedUV : Vector2
 var uv_offset = Vector2.ZERO
+var grid_radius
 
 func _ready():
 	
 	_calculate_uv()
-	var res = _update_mesh(Vector3.ZERO, 0, Vector2())
-	apply_update_to_mesh(res[0], res[1])
+	var res = _update_mesh()
+	apply_update_to_mesh(res[0], res[1], res[2])
+	
+	var mat := StandardMaterial3D.new()
+	mat.albedo_texture = preload("res://assets/textures/checkermap.png")
+	mesh.surface_set_material(0, mat)
 
 
 func _calculate_uv():
 	var hex_pos = get_parent().hex_position
-	var grid_radius = get_parent().get_parent().radius
+	grid_radius = get_parent().get_parent().radius
 	
 	var x = hex_pos.pos.x
-	var y = hex_pos.pos.y
 	var z = hex_pos.pos.z
 	
-	var u = (3.0/2.0 * x + 3.0/2.0 * y + 0.0 * z) / (3.0 * grid_radius)
-	var v = (sqrt(3)/2.0 * x - sqrt(3)/2.0 * y + sqrt(3) * z) / (3.0 * grid_radius)
+	var MAX_ARRAY : Array[Vector3]
+	
+	MAX_ARRAY.append(HexHelper.HexCoordinate.new(0,0,grid_radius).to_carthesian())
+	MAX_ARRAY.append(HexHelper.HexCoordinate.new(grid_radius,0,0).to_carthesian())
+	MAX_ARRAY.append(HexHelper.HexCoordinate.new(0,0,-grid_radius).to_carthesian())
+	MAX_ARRAY.append(HexHelper.HexCoordinate.new(-grid_radius,0,0).to_carthesian())
+	MAX_ARRAY.append(HexHelper.HexCoordinate.new(0,grid_radius,0).to_carthesian())	
+	MAX_ARRAY.append(HexHelper.HexCoordinate.new(0,-grid_radius,0).to_carthesian())
+	
+	var min
+	var max
+		
+		
+		
+	
+	print(uv_offset)
+	
 
-	uv_offset = Vector2(u + 0.5, v + 0.5)
-
-
-func _update_mesh(coord_offset : Vector3 = Vector3.ZERO, idx_offset : int = 0, uv_offset: Vector2 = Vector2.ZERO):
+func _update_mesh(coord_offset : Vector3 = Vector3.ZERO, idx_offset : int = 0):
 	var pts : Array[Vector3] = []
+	
 	
 	pts.append(Vector3.ZERO) #Center
 	
-	pts.append(Vector3(  HexHelper.INNER_RADIUS, 0,  0.5 * HexHelper.OUTER_RADIUS))
+	pts.append(Vector3(  HexHelper.INNER_RADIUS, 0,  0.5 * HexHelper.OUTER_RADIUS), )
 	pts.append(Vector3(  HexHelper.INNER_RADIUS, 0, -0.5 * HexHelper.OUTER_RADIUS))
 	pts.append(Vector3(                       0, 0,       -HexHelper.OUTER_RADIUS))
 	pts.append(Vector3( -HexHelper.INNER_RADIUS, 0, -0.5 * HexHelper.OUTER_RADIUS))
 	pts.append(Vector3( -HexHelper.INNER_RADIUS, 0,  0.5 * HexHelper.OUTER_RADIUS))
 	pts.append(Vector3(                       0, 0,        HexHelper.OUTER_RADIUS))
 	
+	
+	
 	var idx : Array[int] = []
-	
-	
 	
 	
 	for i in range(1, 7):
@@ -48,19 +65,32 @@ func _update_mesh(coord_offset : Vector3 = Vector3.ZERO, idx_offset : int = 0, u
 		else:
 			idx.append(1)
 		idx.append(i)
+		
+		
+	var uvs : Array[Vector2] = []
+	var grid_scale = 1.0 / float(grid_radius * 2)
 	
-	return [pts, idx]
+	for point in pts:		
+		var local_norm_uv = Vector2(point.x, point.z) * grid_scale
+		var new_uv = local_norm_uv + uv_offset
+		
+		uvs.append(new_uv)
+	
+	return [pts, idx, uvs]
 
-func apply_update_to_mesh(vertices, indices):
+func apply_update_to_mesh(vertices, indices, uvs):
 	var packed_coords : PackedVector3Array = PackedVector3Array()
 	packed_coords.append_array(vertices)
 	var packed_idx : PackedInt32Array = PackedInt32Array()
 	packed_idx.append_array(indices)
+	var packed_uvs : PackedVector2Array = PackedVector2Array()
+	packed_uvs.append_array(uvs)
 	
 	var arrays = []
 	arrays.resize(Mesh.ARRAY_MAX)
 	arrays[Mesh.ARRAY_VERTEX] = packed_coords
 	arrays[Mesh.ARRAY_INDEX] = packed_idx
+	arrays[Mesh.ARRAY_TEX_UV] = packed_uvs
 	
 	# Create the Mesh.
 	mesh.clear_surfaces()
