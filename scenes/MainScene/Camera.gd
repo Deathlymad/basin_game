3d99364e803069b8713f41def8353a13fd7104c9
probablysleeping <harker.phillip@gmail.com>
2025-05-08 11:50:46 +0100

    Update Camera.gd
    Fixed Panning

--- scenes/MainScene/Camera.gd
+++ scenes/MainScene/Camera.gd
@@ -11,39 +11,31 @@ var to_camera
 var allowRot := false
 var allowPan := false
 
+var hasInput := false
+
+var input := Vector3.ZERO
+
+
 func _ready():	
 	$Camera3D.look_at(global_transform.origin, Vector3.UP)	
 	
 
 func _unhandled_input(event: InputEvent) -> void:
 	
-	if Input.is_action_pressed("Right_Click") and event.is_pressed():
-		allowRot = true
-	if Input.is_action_pressed("Right_Click") and event.is_released():
-		allowRot = false
-		
-	if Input.is_action_pressed("Middle_Click") and event.is_pressed():
-		allowPan = true
-	if Input.is_action_pressed("Middle_Click") and event.is_released():
-		allowPan = false
+	
 	
 	if event is InputEventMouseMotion and allowRot:
 		rotation.y -= event.relative.x * look_sensitivity
 		rotation.x -= event.relative.y * look_sensitivity
 		rotation.x = clamp(rotation.x, -PI/3, 0)		
 	
-	if event is InputEventMouseMotion and allowPan:
-		var input = Vector3.ZERO
-		if Input.is_action_pressed("forward"):
-			input.z -= 1
-		if Input.is_action_pressed("backward"):
-			input.z += 1
-		if Input.is_action_pressed("left"):
-			input.x -= 1
-		if Input.is_action_pressed("right"):
-			input.x += 1
+	if event is InputEventMouseMotion and allowPan:		
+		input.z = -event.relative.y
+		input.x = -event.relative.x
+		
 			
 		input = input.normalized()
+		hasInput = true
 		
 	
 	
@@ -68,13 +60,19 @@ func _unhandled_input(event: InputEvent) -> void:
 		move_speed = 1;
 
 func _process(delta):
-	
-	
-	
-	
+	if Input.is_action_pressed("Right_Click"):
+		allowRot = true
+	if Input.is_action_just_released("Right_Click") :
+		allowRot = false
+		
+	if Input.is_action_pressed("Middle_Click"):
+		allowPan = true
+		move_speed = 20 * zoom_level;
+	if Input.is_action_just_released("Middle_Click"):
+		allowPan = false
+		move_speed = 1;
 	
 	if !allowPan:
-		var input = Vector3.ZERO
 		if Input.is_action_pressed("forward"):
 			input.z -= 1
 		if Input.is_action_pressed("backward"):
@@ -83,11 +81,11 @@ func _process(delta):
 			input.x -= 1
 		if Input.is_action_pressed("right"):
 			input.x += 1
-			
-		input = input.normalized()
 		
-		var direction = Basis(Vector3.UP, rotation.y) * input
+		input = input.normalized()		
+		hasInput = true
+	
+	if hasInput:
+		var direction = Basis(Vector3.UP, rotation.y) * input	
 		position += direction * move_speed * delta 
-	
-	
-		
+		input = Vector3.ZERO
